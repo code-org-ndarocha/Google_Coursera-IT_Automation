a0adc061cd36b860cbd36dba6888f75975441c17
nikhildarocha <niks77883@gmail.com>
2020-08-01 17:12:27 +0530

    dictionaries

--- practice-quiz-week4.py
+++ practice-quiz-week4.py
@@ -1,33 +1,37 @@
 filenames = ["program.c", "stdio.hpp", "sample.hpp", "a.out", "math.hpp", "hpp.out"]
 # Generate newfilenames as a list containing the new filenames
 # using as many lines of code as your chosen method requires.
-newfilenames = []
-for index, name in enumerate(filenames):
-    print(name)
-    #if name.
-    
+
+
+newfilenames = [x.replace(".hpp",".h") for x in filenames if x.split(".")]
 
 print(newfilenames) 
 # Should be ["program.c", "stdio.h", "sample.h", "a.out", "math.h", "hpp.out"]
 
 
 def pig_latin(text):
-    say = ""
-    words = [text]
-    print(words)
+    say = ""    
+    words = text.split()
     for word in words:
-     return
+        say += word[1:len(word)]+word[0] + "ay "
+        
+        
+    return(say)
+print(pig_latin("hello how are you")) # Should be "ellohay owhay reaay ouyay"
+print(pig_latin("programming in python is fun")) # Should be "rogrammingpay niay ythonpay siay unfay"
+
+
 		
 print(pig_latin("hello how are you")) # Should be "ellohay owhay reaay ouyay"
 print(pig_latin("programming in python is fun")) # Should be "rogrammingpay niay ythonpay siay unfay"
 
 
 def guest_list(guests):
-    	result = []
-	for name, age, prof in guests:
-		result.append("{} is {} years old and works as a {}".format(name, age, prof))
-
-	print(result)
+    result = []
+    for name, age, prof in guests:
+        result.append("{} is {} years old and works as a {}".format(name, age, prof))
+    
+    print(result)
 
 guest_list([('Ken', 30, "Chef"), ("Pat", 35, 'Lawyer'), ('Amanda', 25, "Engineer")])
 
@@ -41,14 +45,8 @@ Amanda is 25 years old and works as Engineer
 
 
 def group_list(group, users):
-    members = []
-    for index, name in enumerate(users):
-        members.append(name)
-    # for name in users:
-    #     members = name.append(group)
-    #     print(members)
-    members = str(members)[1:-1]
-    print("{}: {}".format(group,members))
+    
+    return("{}: {}".format(group,','.join(users)))
 
 print(group_list("Marketing", ["Mike", "Karen", "Jake", "Tasha"])) # Should be "Marketing: Mike, Karen, Jake, Tasha"
 print(group_list("Engineering", ["Kim", "Jay", "Tom"])) # Should be "Engineering: Kim, Jay, Tom"
@@ -57,17 +55,28 @@ print(group_list("Users", "")) # Should be "Users:"
 def octal_to_string(octal):
     result = ""
     value_letters = [(4,"r"),(2,"w"),(1,"x")]
-    
+    # Iterate over each of the digits in octal
     for i in [int(n) for n in str(octal)]:
+        # Check for each of the permissions values
         for value, letter in value_letters:
             if i >= value:
                 result += letter
                 i -= value
             else:
                 result += '-'
-                
-    print(result)  
+    return result
 print(octal_to_string(755)) # Should be rwxr-xr-x
 print(octal_to_string(644)) # Should be rw-r--r--
 print(octal_to_string(750)) # Should be rwxr-x---
 print(octal_to_string(600)) # Should be rw-------
+
+
+
+def guest_list(guests):
+    	
+	for i in guests:
+		name, age, prof = i
+		print("{} is {} years old and works as a {}".format(name, age, prof))
+
+
+guest_list([('Ken', 30, "Chef"), ("Pat", 35, 'Lawyer'), ('Amanda', 25, "Engineer")])
--- tester.py
+++ tester.py
@@ -33,9 +33,10 @@ def pig_latin(text):
     say = ""    
     words = text.split()
     for word in words:
-        say = word.append()
-     return
-		
+        say += word[1:len(word)]+word[0] + "ay "
+        
+        
+    return(say)
 print(pig_latin("hello how are you")) # Should be "ellohay owhay reaay ouyay"
 print(pig_latin("programming in python is fun")) # Should be "rogrammingpay niay ythonpay siay unfay"
 
