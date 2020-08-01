d9189b67f47e09eb42c85b1991dacedbb21bddb5
nikhildarocha <niks77883@gmail.com>
2020-08-01 14:34:20 +0530

    tuples

--- tuple.py
+++ tuple.py
@@ -28,3 +28,41 @@ def file_size(file_info):
 print(file_size(('Class Assignment', 'docx', 17875))) # Should print 17.46
 print(file_size(('Notes', 'txt', 496))) # Should print 0.48
 print(file_size(('Program', 'py', 1239))) # Should print 1.21
+
+
+#Iteration over lists and tuples
+print("Iteration over lists and tuples")
+
+animals = ["Lion", "Zebra", "Dolphin", "Monkey"]
+chars = 0
+for animal in animals:    
+    chars += len(animal)
+    
+    
+print("Total characters: {}, Average length: {}".format(chars, chars/len(animals)))
+
+winners = ["Ashley", "Dylan", "Reese"]
+
+for index, person in enumerate(winners):
+    print("{} - {}".format(index + 1, person))
+
+
+def skip_elements(elements):
+    new_list = []
+    for index, alphabet in enumerate(elements):
+        if index%2 == 0:
+            new_list.append(alphabet)
+    return new_list
+    
+
+print(skip_elements(["a", "b", "c", "d", "e", "f", "g"])) # Should be ['a', 'c', 'e', 'g']
+print(skip_elements(['Orange', 'Pineapple', 'Strawberry', 'Kiwi', 'Peach'])) # Should be ['Orange', 'Strawberry', 'Peach']
+
+def full_emails(people):
+    result = []
+    for email, name in people:
+        result.append("{} <{}>".format(name, email))
+    return result
+
+print(full_emails([("alex@example.com","Alex Diego"), ("shay@example.com", "Shay Brandt")]))
+    
