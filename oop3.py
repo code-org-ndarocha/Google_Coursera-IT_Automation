f0706ed4e907bfc7ec9b52cfe296bf18d84d8eb1
nikhildarocha <niks77883@gmail.com>
2020-08-04 21:56:02 +0530

    @ndarocha updating

--- oop3.py
+++ oop3.py
@@ -24,3 +24,24 @@ print(some_person.greeting())
 
 
 print(jonagold)
+
+help(Apple)
+
+print("Docstring!")
+
+def to_seconds(hours, minutes, seconds):
+    """Return the amount of seconds in the given hours, minutes and seconds."""
+    return hours*3600+minutes*60+seconds
+
+
+help(to_seconds)
+
+
+class Person:
+    def __init__(self, name):
+        self.name = name
+    def greeting(self):
+        """Outputs a message with the name of the person."""
+        print("Hello! My name is {name}.".format(name=self.name)) 
+
+help(Person)
