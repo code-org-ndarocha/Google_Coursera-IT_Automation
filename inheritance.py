49ae8bbac01db99a5a33909cd583a3d1f4fc3a69
nikhildarocha <niks77883@gmail.com>
2020-08-04 22:49:53 +0530

    inheritance

---
+++ inheritance.py
@@ -0,0 +1,51 @@
+class Fruit:
+    def __init__(self,color, flavor):
+        self.color = color
+        self.flavor = flavor
+        
+class Apple(Fruit):
+    pass
+
+class Grape(Fruit):
+    pass
+
+granny_smith = Apple("green", "tart")
+carnelian = Grape("purple", "sweet")
+
+print(granny_smith.color)
+print(carnelian.flavor)
+
+
+
+class Animal:
+    sound = ""
+    def __init__(self, name):
+        self.name = name
+    def speak(self):
+        print("{sound} I'm {name}! {sound}".format(name=self.name, sound=self.sound))
+
+class Piglet(Animal):
+    sound = "Oink!"
+
+hamlet = Piglet("Hamlet")
+hamlet.speak()
+
+class Cow(Animal):
+    sound = "Moooooooo"
+    
+milky = Cow("Milky White")
+milky.speak()
+
+
+class Clothing:
+    material = ""
+    def __init__(self,name):
+        self.name = name
+    def checkmaterial(self):
+        print("This {} is made of {}".format(self.name,self.material))
+			
+class Shirt(Clothing):
+  material="Cotton"
+
+polo = Shirt("Polo")
+polo.checkmaterial()        
